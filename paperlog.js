33eda853aef1576b903009d3f8646a84cee94d47
r3dDoX <Patrick@Elfriede-Pro.local>
2014-02-13 11:02:33 +0100

    Added functionality to show input on click and submit change on blur

--- paperlog.js
+++ paperlog.js
@@ -2,8 +2,50 @@ Profiles = new Meteor.Collection("profiles");
 
 if (Meteor.isClient) {
   Template.profile.selectedProfile = function () {
-		return Profiles.findOne({/* TODO select profile with param */}, {fields: {_id: 0}});
+		var selectedProfile = Profiles.findOne({/* TODO select profile with param */});
+
+		if(selectedProfile) {
+			Session.set('selectedProfile', selectedProfile._id);
+		}
+		
+		return selectedProfile;
   };
+	
+	Template.profile.events({
+		'click span': function(event) {
+			toggleInput(event.target);
+		},
+		
+		'submit form': function(event) {
+			event.target.getElementsByTagName('input')[0].blur();
+			return false;
+		}
+	});
+	
+	function toggleInput(spanElement) {
+		var value = spanElement.innerHTML,
+				name = spanElement.getAttribute('data-name'),
+				parentNode = spanElement.parentNode,
+				inputElement = undefined,
+				spanElementDisplay = spanElement.style.display;
+		
+		spanElement.style.display = 'none';
+		parentNode.insertAdjacentHTML('beforeend', Template.input({name: name, value: value}));
+		inputElement = parentNode.getElementsByTagName('input')[0];
+		inputElement.focus();
+		
+		inputElement.addEventListener('blur', function(event) {
+			if(inputElement.value) {
+				var updateObject = {};
+				updateObject[name] = inputElement.value;
+
+				Profiles.update({_id: Session.get('selectedProfile')}, updateObject);
+			}
+			
+			parentNode.removeChild(inputElement);
+			spanElement.style.display = spanElementDisplay;
+		});
+	}
 }
 
 if (Meteor.isServer) {
